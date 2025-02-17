34229a831b0964670423f33e84da362e2c5ab23c
Quang Doan <quang.m.doan@gmail.com>
2025-02-17 13:17:41 -0800

    Simple Arcade drive code.

--- src/main/java/frc/robot/subsystems/Drivebase.java
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -8,142 +8,49 @@ import com.revrobotics.spark.SparkMax;
 import com.studica.frc.AHRS;
 import com.studica.frc.AHRS.NavXComType;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.Constants.DriveConstants;
-import frc.robot.Constants.DriveConstants.AutoAlign;
-import poplib.smart_dashboard.PIDTuning;
 
 public class Drivebase extends SubsystemBase {
-  private SparkMax leadRightMotor;
-  private SparkMax followRightMotor;
-  private SparkMax leadLeftMotor;
-  private SparkMax followLeftMotor;
+  private final SparkMax leadRightMotor;
+  private final SparkMax followRightMotor;
+  private final SparkMax leadLeftMotor;
+  private final SparkMax followLeftMotor;
 
-  private final DifferentialDrive drive;
+  private final DifferentialDrive differentialDrive;
+  private final DifferentialDriveOdometry odometry;
 
-  private DifferentialDriveKinematics kinematics;
-  private DifferentialDriveOdometry odometry;
-  private AHRS gyro;
-  private static PIDController xpid;
-  private static PIDController thetapid;
-  protected PIDTuning leftTuning;
-  protected PIDTuning rightTuning;
-  private static Drivebase instance;
-  private Pose2d currentPose;
-  private double X;
-  private double Y;
-  private double theta;
-
-
+  // TODO: Check this with hardware person about the gyro sensor com type
+  private final AHRS gyro = new AHRS(NavXComType.kMXP_SPI);
+  
+  private static Drivebase driveSubystem;
   public static Drivebase getDriveSubystem() {
-    if (instance == null) {
-        instance = new Drivebase();
+    if (driveSubystem == null) {
+        driveSubystem = new Drivebase();
       }
-    return instance;
+    return driveSubystem;
   }
 
   public Drivebase() {
     leadLeftMotor = DriveConstants.LeadLeftMotor.createSparkMax();
     followLeftMotor = DriveConstants.FollowLeftMotor.createSparkMax();
     leadRightMotor = DriveConstants.LeadRightMotor.createSparkMax();
-    followRightMotor = DriveConstants.FollowRightMotor.createSparkMax();
+    followRightMotor = DriveConstants.FollowRightMotor.createSparkMax();    
+    differentialDrive = new DifferentialDrive(leadLeftMotor, leadRightMotor);    
 
-    this.rightTuning = new PIDTuning("Right Motors", DriveConstants.LeadRightMotor.pid, DriveConstants.PID_TUNING_MODE);
-    this.leftTuning = new PIDTuning("Left Motors", DriveConstants.LeadLeftMotor.pid, DriveConstants.PID_TUNING_MODE);
-    
-    kinematics = new DifferentialDriveKinematics(DriveConstants.TRACK_WIDTH);
-    
-    gyro = new AHRS(NavXComType.kMXP_SPI);
     gyro.reset();
 
-    xpid.setTolerance(AutoAlign.X_TOLERANCE);
-    thetapid.setTolerance(AutoAlign.THETA_TOLERANCE);
-    currentPose = new Pose2d();
-
-    odometry = new DifferentialDriveOdometry
-    (gyro.getRotation2d(), leadLeftMotor.getEncoder().getPosition(), 
-    leadRightMotor.getEncoder().getPosition(), new Pose2d(0,0, new Rotation2d()));
-
-    xpid = DriveConstants.X_PID_CONTROLLER;
-    thetapid = DriveConstants.THETA_PID_CONTROLLER;
-    drive = new DifferentialDrive(leadLeftMotor, leadRightMotor);
-  }
-
-  public Command moveToPose(Pose2d targetPose2d) {
-     return runOnce(() -> {
-      X = Math.abs(currentPose.getX() - targetPose2d.getX());
-      Y = Math.abs(currentPose.getY() - targetPose2d.getY());
-      theta = Math.atan(Y/X);
-     }).andThen(
-      turnToPose(theta)
-     ).andThen(
-      driveToPose(targetPose2d)
-     );
-  }
-
-  public Command turnToPose(double angle){
-    return runOnce(() -> {
-      thetapid.setSetpoint(0);
-      thetapid.calculate(gyro.getAngle());
-    }).andThen(run(() -> {
-      leadLeftMotor.set(thetapid.calculate(gyro.getAngle())*DriveConstants.DRIVE_MOVE_SPEED);
-    })).alongWith(run(() -> {
-      leadRightMotor.set(thetapid.calculate(gyro.getAngle())*DriveConstants.DRIVE_REVERSE_SPEED);
-    })).until(thetapid::atSetpoint).
-    andThen(runOnce(() -> {
-      leadLeftMotor.stopMotor();
-      leadRightMotor.stopMotor();
-      thetapid.close();
-    }));
-  }
-
-  public Command driveToPose(Pose2d targetPose){
-    return runOnce(() -> {
-      xpid.setSetpoint(0.0);
-      X = Math.abs(currentPose.getX() - targetPose.getX());
-      Y = Math.abs(currentPose.getY() - targetPose.getY());
-      xpid.calculate(Math.hypot(X, Y));
-    }).andThen(run( () -> {
-      X = Math.abs(currentPose.getX() - targetPose.getX());
-      Y = Math.abs(currentPose.getY() - targetPose.getY());
-      leadLeftMotor.set(xpid.calculate(Math.hypot(X, Y)));
-      leadRightMotor.set(xpid.calculate(Math.hypot(X, Y)));
-    })).until(xpid::atSetpoint)
-    .andThen(runOnce(() -> {
-      xpid.close();
-      leadLeftMotor.stopMotor();
-      leadRightMotor.stopMotor();
-    }));
-  }
-
-  public Command moveForward(){
-    return run(()->{
-      leadLeftMotor.set(-1);
-      leadRightMotor.set(-1);
-    });
-  }
-
-  public Command reverseCommand(){
-    return run(()-> {
-      leadLeftMotor.set(Constants.DriveConstants.DRIVE_REVERSE_SPEED);
-      leadRightMotor.set(Constants.DriveConstants.DRIVE_REVERSE_SPEED);
-    });
-  }
-    
-  public Command stopCommand(){
-    return runOnce(() -> {
-      leadLeftMotor.set(Constants.DriveConstants.STOP_SPEED);
-      leadRightMotor.set(Constants.DriveConstants.STOP_SPEED);
-    });
+    // TODO: Make sure when the Drivebase is created, the initialRotation, leftDistanceMeters, rightDistanceMeters are setting properly
+    Rotation2d initialRotation = gyro.getRotation2d();
+    double leftDistanceMeters = 0;
+    double rightDistanceMeters = 0;
+    odometry = new DifferentialDriveOdometry(initialRotation, leftDistanceMeters, rightDistanceMeters);
+    SmartDashboard.putString("odometry started at: ", initialRotation.toString() + ", leftDistanceMeters: " + leftDistanceMeters + ", rightDistanceMeters = " + rightDistanceMeters);
   }
 
   public void updateOdometry() {
@@ -157,13 +64,11 @@ public class Drivebase extends SubsystemBase {
   }
 
   public void driveArcade(double xSpeed, double zRotation) {
-    drive.arcadeDrive(xSpeed, zRotation);
+    differentialDrive.arcadeDrive(xSpeed, zRotation);
   }
 
   @Override
   public void periodic() {
-    leftTuning.updatePID(leadLeftMotor);
-    rightTuning.updatePID(leadRightMotor);
     updateOdometry();
     Pose2d pose = odometry.getPoseMeters();
     SmartDashboard.putNumber("Robot pose: x", pose.getX());
