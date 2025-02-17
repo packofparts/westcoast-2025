52a732744c2d6177c33f5738d453cb4ba323e719
Quang Doan <quang.m.doan@gmail.com>
2025-02-17 13:10:51 -0800

    Robot Code commands.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,26 +3,31 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.DriveCommand;
 import frc.robot.subsystems.Drivebase;
+import poplib.controllers.oi.OI;
+import poplib.controllers.oi.XboxOI;
+import edu.wpi.first.wpilibj2.command.Command;
 
 
 public class RobotContainer {
 
-  private final Drivebase driveSubSystem = Drivebase.getDriveSubystem();
+  private final Drivebase driveSubsystem = Drivebase.getDriveSubystem();
+  private final XboxController driveController = new XboxController(0);
+  public final OI oi;
   
   //TODO: Make sure xbox connecting to port OperatorConstants.kDriverControllerPort = 0
-  private final CommandXboxController driveController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   public RobotContainer() {
+    oi = new XboxOI();
     configureBindings();
   }
   
   private void configureBindings() {
-    Command cmd = new DriveCommand( () -> -driveController.getLeftY(),  () -> -driveController.getRightX(), driveSubSystem);
-    driveSubSystem.setDefaultCommand(cmd);
+    Command cmd = new DriveCommand(() -> -driveController.getLeftY() , ()-> -driveController.getRightX(), driveSubsystem);
+    driveSubsystem.setDefaultCommand(cmd);
+    // oi.getDriverButton(XboxController.Button.kA.value).onTrue(driveSubSystem.moveForward()).onFalse(driveSubSystem.stopCommand());
   }
 }
